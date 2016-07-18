6330cd95ce6eee4fcee53365cfcae50069b1e02b
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2016-07-18 15:19:48 -0400

    Initialize repo

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// LateUpdate is called once per frame, but only after Update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		// Set to Rigidbody component on Player object
+		rb = GetComponent<Rigidbody> ();
+
+		// Initialize score count
+		count = 0;
+		SetCountText ();
+
+		// Initialize text message for win
+		winText.text = "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// FixedUpdate is called before performing any physics calculation
+	void FixedUpdate () {
+		// Capture X/Z axis movement from user input
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Add user movement to the Rigidbody
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	// Called when confronted with a trigger object
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 16) {
+			winText.text = "You Win!";
+		}
+	}
+}
